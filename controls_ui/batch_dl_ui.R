896829bf11e1352e7ec4139a9ff2bc77d24bd15b
Bill DeVoe <39626006+bdevoe@users.noreply.github.com>
2023-03-21 15:23:47 -0400

    Cleaning up

--- controls_ui/batch_dl_ui.R
+++
@@ -1,8 +0,0 @@
-### UI for batch download buttons
-batch_dl_ui <- tags$div(
-  # Download buttons
-  downloadButton(outputId = "dl_all_mod", 
-                 label = "Modern Landings"),
-  downloadButton(outputId = "dl_all_hist", 
-                 label = "Historic Landings")
-)
