698ea7faf89e1048d0ed1027cd6a71ffeeb89eec
Bill DeVoe <39626006+bdevoe@users.noreply.github.com>
2023-05-16 08:41:38 -0400

    Add text with slope to plots

--- tab_ts/tab_ts_srv.R
+++ tab_ts/tab_ts_srv.R
@@ -50,48 +50,86 @@ output$ts_plot <- renderPlot({
   ylab_title <- glue('Average Sea Surface Temperature ({temp_label()})')
   
   if (input$ts_groupby == 'None') {
+    slope_data <- yearly_dataset() %>%
+      summarize(slope = summary(lm(avg_temp ~ year))$coefficients[2],
+                x = mean(year, na.rm = T)) %>%
+      mutate(label_text = paste0("Trend = ", round(slope, 2), temp_label(), ' per Year'))
     p <- ggplot(yearly_dataset(), aes(x = year, y = avg_temp)) + 
       geom_line() +
       geom_point() +
       geom_smooth(method = 'lm', color = "blue") +
       ylab(ylab_title) +
-      xlab("Year")
+      xlab("Year") +
+      geom_text(data = slope_data, aes(x = x, y = Inf, label = label_text),
+                hjust = 1, vjust = 1, color = "black")
     print(p)
   } else if (input$ts_groupby == 'Month') {
+    slope_data <- monthly_dataset() %>%
+      group_by(month) %>%
+      summarize(slope = summary(lm(avg_temp ~ year))$coefficients[2],
+                x = mean(year, na.rm = T)) %>%
+      mutate(label_text = paste0("Trend = ", round(slope, 2), temp_label(), ' per Year'))
+    
     p <- ggplot(monthly_dataset(), aes(x = year, y = avg_temp)) + 
       geom_line() +
       geom_point() +
       facet_wrap(~month, nrow = 4) +
       geom_smooth(method = 'lm', color = "blue") +
       ylab(ylab_title) +
-      xlab("Year")
+      xlab("Year") +
+      geom_text(data = slope_data, aes(x = x, y = Inf, label = label_text),
+                hjust = 1, vjust = 1, color = "black")
     print(p)
   } else if (input$ts_groupby == 'Oceanic Season') {
+    slope_data <- ocean_season_dataset() %>%
+      group_by(ocean_season) %>%
+      summarize(slope = summary(lm(avg_temp ~ ocean_year))$coefficients[2],
+                x = mean(ocean_year, na.rm = T)) %>%
+      mutate(label_text = paste0("Trend = ", round(slope, 2), temp_label(), ' per Year'))
+    
     p <- ggplot(ocean_season_dataset(), aes(x = ocean_year, y = avg_temp)) + 
       geom_line() +
       geom_point() +
       facet_wrap(~ocean_season, nrow = 2) +
       geom_smooth(method = 'lm', color = "blue") +
       ylab(ylab_title) +
-      xlab("Ocean Year")
+      xlab("Ocean Year") +
+      geom_text(data = slope_data, aes(x = x, y = Inf, label = label_text),
+                hjust = 1, vjust = 1, color = "black")
     print(p)
   } else if (input$ts_groupby == 'Meteorological Season') {
+    slope_data <- met_season_dataset() %>%
+      group_by(met_season) %>%
+      summarize(slope = summary(lm(avg_temp ~ met_year))$coefficients[2],
+                x = mean(met_year, na.rm = T)) %>%
+      mutate(label_text = paste0("Trend = ", round(slope, 2), temp_label(), ' per Year'))
+    
     p <- ggplot(met_season_dataset(), aes(x = met_year, y = avg_temp)) + 
       geom_line() +
       geom_point() +
       facet_wrap(~met_season, nrow = 2) +
       geom_smooth(method = 'lm', color = "blue") +
       ylab(ylab_title) +
-      xlab("Meteorological Year")
+      xlab("Meteorological Year") +
+      geom_text(data = slope_data, aes(x = x, y = Inf, label = label_text),
+                hjust = 1, vjust = 1, color = "black")
     print(p)
   } else if (input$ts_groupby == 'Astronomical Season') {
+    slope_data <- astro_season_dataset() %>%
+      group_by(astro_season) %>%
+      summarize(slope = summary(lm(avg_temp ~ astro_year))$coefficients[2],
+                x = mean(astro_year, na.rm = T)) %>%
+      mutate(label_text = paste0("Trend = ", round(slope, 2), temp_label(), ' per Year'))
+    
     p <- ggplot(astro_season_dataset(), aes(x = astro_year, y = avg_temp)) + 
       geom_line() +
       geom_point() +
       facet_wrap(~astro_season, nrow = 2) +
       geom_smooth(method = 'lm', color = "blue") +
       ylab(ylab_title) +
-      xlab("Astronomical Year")
+      xlab("Astronomical Year") +
+      geom_text(data = slope_data, aes(x = x, y = Inf, label = label_text),
+                hjust = 1, vjust = 1, color = "black")
     print(p)
   }
 }, height = 700)
